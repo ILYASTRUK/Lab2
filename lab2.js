df0ce0852955e46575af3304101f5711d3c1aac8
ILYASTRUK <43352343+ILYASTRUK@users.noreply.github.com>
2018-12-22 00:17:36 +0300

    Add files via upload

---
+++ lab2.js
@@ -0,0 +1,9 @@
+$(function() {
+        var pull= $('#pull');
+            menu= $('nav ul');
+            menuHeight  = menu.height();
+        $(pull).on('click', function(e) {
+            e.preventDefault();
+            menu.slideToggle();
+        });
+    });    
